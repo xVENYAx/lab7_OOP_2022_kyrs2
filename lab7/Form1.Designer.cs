da4d29808b08cf2d5388eaddb0a997b119ca5496
Sasha <t3t3t3@ukr.net>
2022-05-11 10:05:33 +0300

    Добавьте файлы проекта.

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_street = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label_door = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label_train = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_street);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 45);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вулиця";
+            // 
+            // label_street
+            // 
+            this.label_street.AutoSize = true;
+            this.label_street.Location = new System.Drawing.Point(6, 18);
+            this.label_street.Name = "label_street";
+            this.label_street.Size = new System.Drawing.Size(82, 17);
+            this.label_street.TabIndex = 0;
+            this.label_street.Text = "label_street";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.label_door);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 63);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 79);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Двері";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(99, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(41, 51);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label_door
+            // 
+            this.label_door.AutoSize = true;
+            this.label_door.Location = new System.Drawing.Point(18, 34);
+            this.label_door.Name = "label_door";
+            this.label_door.Size = new System.Drawing.Size(75, 17);
+            this.label_door.TabIndex = 0;
+            this.label_door.Text = "label_door";
+            this.label_door.TextChanged += new System.EventHandler(this.label_door_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(99, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox5);
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 148);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(231, 225);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Приміщення вокзалу";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Location = new System.Drawing.Point(8, 129);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(217, 87);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Каси";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "label5";
+            this.label5.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            this.label4.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            this.label3.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            this.label2.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(42, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(99, 84);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label_train);
+            this.groupBox4.Location = new System.Drawing.Point(13, 555);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(230, 71);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Потяг";
+            // 
+            // label_train
+            // 
+            this.label_train.AutoSize = true;
+            this.label_train.Location = new System.Drawing.Point(9, 35);
+            this.label_train.Name = "label_train";
+            this.label_train.Size = new System.Drawing.Size(74, 17);
+            this.label_train.TabIndex = 0;
+            this.label_train.Text = "label_train";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.label6);
+            this.groupBox6.Controls.Add(this.label1);
+            this.groupBox6.Controls.Add(this.listBox2);
+            this.groupBox6.Location = new System.Drawing.Point(13, 379);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(230, 170);
+            this.groupBox6.TabIndex = 4;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Платформа";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.label3_TextChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(17, 31);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(201, 100);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(141, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            this.label6.TextChanged += new System.EventHandler(this.label3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 633);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label_street;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_door;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label_train;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ lab7/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace lab7
+{
+        public partial class Form1 : Form
+        {
+            Mutex mut = new Mutex();
+            Semaphore sem = new Semaphore(4, 10);
+            Semaphore plat = new Semaphore(2, 3);
+            List<Label> labels = new List<Label>();
+            List<Label> labels1 = new List<Label>();
+            int maxPersons = 10;
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                Control.CheckForIllegalCrossThreadCalls = false; // вимикаємо перевірку в Debug
+
+                labels.AddRange(new[] { label2, label3, label4, label5 });
+                foreach (Label item in labels)
+                {
+                    item.Text = "X";
+                }
+
+                labels1.AddRange(new[] { label1, label6 });
+                foreach(Label item1 in labels1)
+                {
+                   item1.Text = "X";
+                }
+
+                label_train.Text = "";
+                label_street.Text = "";
+                for (int i = 0; i < maxPersons; i++)
+                {
+                    label_street.Text += i.ToString() + ", ";
+                }
+
+                for (int i = 0; i < maxPersons; i++)
+                {
+                    Thread myThread = new Thread(changeLabelDoor);
+                    string name = i.ToString();
+                    myThread.Name = name + " людина біля дверей";
+                    myThread.Start(name);
+                }
+            }
+
+            void changeLabelDoor(object obj)
+            {
+                bool flag = false;
+                while (true)
+                {
+                    if (obj is string str)
+                    {
+                        mut.WaitOne();
+
+                        //Console.WriteLine($"    {Thread.CurrentThread.Name} заглянула до зали");
+                        label_street.Text = label_street.Text.Replace(str, "x");
+                        label_door.Text = str;
+                        Thread.Sleep(1000);
+                        Thread myThread = new Thread(changeLabelsHall);
+                        Console.WriteLine($"    {Thread.CurrentThread.Name} зайшла до зали");
+                        myThread.Name = str + " людина у залі";
+                        label_door.Text = "X";
+                        listBox1.Items.Add(str);
+                        Thread.Sleep(1000);
+                        myThread.Start(str);
+
+                        flag = true;
+
+                        mut.ReleaseMutex();
+
+                        if (flag == true) return; //Thread.CurrentThread.Abort();
+                    }
+                }
+            }
+
+            void changeLabelsHall(object obj)
+            {
+                bool flag = false;
+                while (true)
+                {
+                    sem.WaitOne();
+
+                    if (obj is string str)
+                    {
+                        foreach (Label item in labels)
+                        {
+                            if (item.Text == "X")
+                            {
+                                Console.WriteLine($"{Thread.CurrentThread.Name} знайшла вільну касу");
+                                listBox1.Items.Remove(str);
+                                item.Text = str;
+                                Thread.Sleep(10000);
+                                //Console.WriteLine($"{Thread.CurrentThread.Name} вийшла з зали");
+                                item.Text = "X";
+                                Thread myThread1 = new Thread(changeLabelsPlatform);
+                                listBox2.Items.Add(str);
+                                myThread1.Start(str);
+                                //label_train.Text += str + ", ";
+                                Thread.Sleep(1000);
+                                flag = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    sem.Release();
+
+                    if (flag == true) return; // Thread.CurrentThread.Abort();
+                }
+            }
+
+        void changeLabelsPlatform(object obj)
+        {
+            bool flag = false;
+            while (true)
+            {
+                plat.WaitOne();
+
+                if (obj is string str)
+                {
+                    foreach (Label item1 in labels1)
+                    {
+                        if (item1.Text == "X")
+                        {
+                            Console.WriteLine($"{Thread.CurrentThread.Name} знайшла відкриті двері");
+                            item1.Text = str;
+                            listBox2.Items.Remove(str);
+                            Thread.Sleep(7500);
+                            //Console.WriteLine($"{Thread.CurrentThread.Name} вийшла з зали");
+                            item1.Text = "X";
+                            label_train.Text += str + ", ";
+                            Thread.Sleep(750);
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+
+                plat.Release();
+
+                if (flag == true) return; // Thread.CurrentThread.Abort();
+            }
+        }
+
+
+            private void label2_TextChanged(object sender, EventArgs e)
+            {
+                foreach (Label item in labels)
+                {
+                    item.BackColor = SystemColors.Control;
+                    if (item == (Label)sender)
+                    {
+                        item.BackColor = Color.LightGreen;
+                    }
+                }
+            }
+
+        private void label3_TextChanged(object sender, EventArgs e)
+        {
+            foreach (Label item1 in labels1)
+            {
+                item1.BackColor = SystemColors.Control;
+                if (item1 == (Label)sender)
+                {
+                    item1.BackColor = Color.LightGreen;
+                }
+            }
+        }
+
+        private void label_door_TextChanged(object sender, EventArgs e)
+        {
+            if (label_door.Text != "X")
+            {
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = true;
+            }
+            if (label_door.Text == "X")
+            {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+            }
+        }
+
+        
+    }
+    }
